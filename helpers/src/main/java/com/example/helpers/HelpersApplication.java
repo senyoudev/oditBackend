37fa34d3876f591844cf265971ceff7c32768bae
Yassine-Chraa <yassinechraa1@gmail.com>
2023-07-04 16:21:59 +0100

    update

---
+++ helpers/src/main/java/com/example/helpers/HelpersApplication.java
@@ -0,0 +1,14 @@
+package com.example.helpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpersApplication.class, args);
+    }
+
+}
