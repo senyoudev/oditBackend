04116a7f25ff13d87b3acc6d9ae0f1d86fd957cf
senyoudev <younesmeskafe2018@gmail.com>
2023-07-18 18:39:00 +0100

    unit test for room repository

--- room-service/src/test/java/com/example/roomservice/RoomServiceApplicationTests.java
+++ room-service/src/test/java/com/example/roomservice/RoomServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.roomservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class RoomServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
 
 }
