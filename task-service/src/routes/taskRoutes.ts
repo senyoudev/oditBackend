3df11031f5ef84ed8054ff002e0903f4d7016862
Yassine-Chraa <yassinechraa1@gmail.com>
2023-07-28 05:59:44 +0100

    update task service

--- task-service/src/routes/taskRoutes.ts
+++ task-service/src/routes/taskRoutes.ts
@@ -1,14 +1,24 @@
-import express from 'express';
-import { assignTask, createTask, deleteTask, getTaskById, markTaskDone, updateTask } from '../controllers/taskController'
-
+import express from "express";
+import {
+  addComment,
+  assignTask,
+  createTask,
+  deleteComment,
+  deleteTask,
+  getTaskById,
+  markTaskDone,
+  updateTask,
+} from "../controllers/taskController";
 
 const router = express.Router();
 
-router.get('/:taskId',getTaskById);
-router.post('/',createTask);
-router.put('/:taskId',updateTask);
-router.delete('/:taskId',deleteTask);
-router.put('/:taskId/assign',assignTask);
-router.put('/:taskId/done',markTaskDone);
+router.get("/:taskId", getTaskById);
+router.post("/", createTask);
+router.put("/:taskId", updateTask);
+router.delete("/:taskId", deleteTask);
+router.put("/:taskId/assign", assignTask);
+router.put("/:taskId/done", markTaskDone);
+router.put("/:taskId/addcomment", addComment);
+router.put("/:taskId/deletecomment", deleteComment);
 
 export default router;
