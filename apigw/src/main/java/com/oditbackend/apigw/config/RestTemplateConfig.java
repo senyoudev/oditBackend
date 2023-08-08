488235b05ee5c36c56c3fe47a6a358d1bc1a674f
Yassine-Chraa <yassinechraa1@gmail.com>
2023-08-08 03:51:05 +0100

    update

--- apigw/src/main/java/com/oditbackend/apigw/config/RestTemplateConfig.java
+++ apigw/src/main/java/com/oditbackend/apigw/config/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package com.oditbackend.apigw.config;
 
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
